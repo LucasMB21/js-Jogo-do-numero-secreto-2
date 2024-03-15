b0b832143644eab83eefc0f28eed224bfacd4641
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-14 23:21:15 -0300

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let numeroSecreto = gerarNumeroAleatorio();
+
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -7,5 +9,10 @@ exibirTextoNaTela('h1' , 'Jogo do número secreto');
 exibirTextoNaTela('p' , 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('O botão foi clicado');
+    let chute = document.querySelector('input').value;
+    console.log(chute == numeroSecreto);
+}
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
 }
