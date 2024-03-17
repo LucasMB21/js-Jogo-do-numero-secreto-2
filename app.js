3e48e617e7bbffd3e83958ac15faf77181ea51df
Lucas Rodrigues <lucas.r.c2010@hotmail.com>
2024-03-17 20:27:16 -0300

    update  app.js
    foi acrescentado a quantidade de números para 50.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 50;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -11,7 +11,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1' , 'Jogo do número secreto');
-    exibirTextoNaTela('p' , 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p' , 'Escolha um número entre 1 e 50');
 }
 
 exibirMensagemInicial();
